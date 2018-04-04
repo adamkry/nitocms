02a75460cb2b29d3ac55cba5dd347bb3b099f761
Adam Krygowski <Adam_Krygowski@epam.com>
2018-04-04 11:38:45 +0200

    Im coming up

---
+++ nitocms/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using docxtools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nitocms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            if (postedFile != null)
+            {
+                string path = Server.MapPath("~/Uploads/BlogPosts/docx");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                var resultFileName = Path.Combine(path, Path.GetFileName(postedFile.FileName));
+                postedFile.SaveAs(resultFileName);
+
+                DocxTools.ConvertToHtml(resultFileName, Path.Combine(path, "html"));
+
+                ViewBag.Message = "File uploaded successfully.";
+            }
+
+            return View();
+        }
+    }
+}
