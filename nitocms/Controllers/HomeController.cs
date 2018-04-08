207112360ac44394e879ae36b955d3c718f5599e
Adam Krygowski <adam.krygowski@gmail.com>
2018-04-08 12:28:09 +0200

    catch exceptions

--- nitocms/Controllers/HomeController.cs
+++ nitocms/Controllers/HomeController.cs
@@ -22,58 +22,65 @@ namespace nitocms.Controllers
 
         [HttpPost]
         public async Task<ActionResult> Index(HttpPostedFileBase postedFile)
-        {            
-            if (postedFile != null)
+        {
+            try
             {
-                var blogPostId = Guid.NewGuid();
-                string path = Server.MapPath($"~/Uploads/BlogPosts/{blogPostId}");
-                if (!Directory.Exists(path))
+                if (postedFile != null)
                 {
-                    Directory.CreateDirectory(path);
-                }
-
-                var uploadedFileName = Path.GetFileName(postedFile.FileName);
-                var uploadedFilePath = Path.Combine(path, Path.GetFileName(postedFile.FileName));
-                postedFile.SaveAs(uploadedFilePath);
-
-                var htmlDirectoryPath = Path.Combine(path, "html");
-                DocxTools.ConvertToHtml(uploadedFilePath,
-                    htmlDirectoryPath,
-                    $"/images/blogposts/{blogPostId}/",
-                    imgNo => $"image" + imgNo.ToString());
-
-                string htmlFileName = Path.ChangeExtension(uploadedFileName, ".html");
-                string htmlFilePath = Path.Combine(htmlDirectoryPath, htmlFileName);
-
-                
-
-                using (var client = new HttpClient())
-                {
-#if DEBUG
-                    targetUrl = "http://localhost:5000";
-#endif
-                    var api = new BlogPostApi(client, targetUrl);
-                    string fileName = Path.GetFileNameWithoutExtension(htmlFileName);
-                    var wholeHtml = System.IO.File.ReadAllText(htmlFilePath);
-                    var page = HtmlPageParser.Parse(wholeHtml);
-
-                    await api.SendBlogPostAsync(new CreateBlogPostViewModel
+                    var blogPostId = Guid.NewGuid();
+                    string path = Server.MapPath($"~/Uploads/BlogPosts/{blogPostId}");
+                    if (!Directory.Exists(path))
                     {
-                        Id = blogPostId,
-                        Title = fileName,
-                        Content = page.HtmlContent,
-                        TextContent = page.TextContent,
-                        Styles = page.CssStyles
-                    });
-                    var images = Directory.GetFiles(Path.Combine(htmlDirectoryPath, fileName + "_files"));
-                    foreach (var imagePath in images)
-                    {
-                        var imageContent = System.IO.File.ReadAllBytes(imagePath);
-                        WebApi.PostFile(client, blogPostId, imageContent, Path.GetFileName(imagePath));
+                        Directory.CreateDirectory(path);
                     }
-                }
 
-                ViewBag.Message = $"File uploaded successfully ({blogPostId})";
+                    var uploadedFileName = Path.GetFileName(postedFile.FileName);
+                    var uploadedFilePath = Path.Combine(path, Path.GetFileName(postedFile.FileName));
+                    postedFile.SaveAs(uploadedFilePath);
+
+                    var htmlDirectoryPath = Path.Combine(path, "html");
+                    DocxTools.ConvertToHtml(uploadedFilePath,
+                        htmlDirectoryPath,
+                        $"/images/blogposts/{blogPostId}/",
+                        imgNo => $"image" + imgNo.ToString());
+
+                    string htmlFileName = Path.ChangeExtension(uploadedFileName, ".html");
+                    string htmlFilePath = Path.Combine(htmlDirectoryPath, htmlFileName);
+
+
+
+                    using (var client = new HttpClient())
+                    {
+#if DEBUG
+                        targetUrl = "http://localhost:5000";
+#endif
+                        var api = new BlogPostApi(client, targetUrl);
+                        string fileName = Path.GetFileNameWithoutExtension(htmlFileName);
+                        var wholeHtml = System.IO.File.ReadAllText(htmlFilePath);
+                        var page = HtmlPageParser.Parse(wholeHtml);
+
+                        await api.SendBlogPostAsync(new CreateBlogPostViewModel
+                        {
+                            Id = blogPostId,
+                            Title = fileName,
+                            Content = page.HtmlContent,
+                            TextContent = page.TextContent,
+                            Styles = page.CssStyles
+                        });
+                        var images = Directory.GetFiles(Path.Combine(htmlDirectoryPath, fileName + "_files"));
+                        foreach (var imagePath in images)
+                        {
+                            var imageContent = System.IO.File.ReadAllBytes(imagePath);
+                            WebApi.PostFile(client, blogPostId, imageContent, Path.GetFileName(imagePath));
+                        }
+                    }
+
+                    ViewBag.Message = $"File uploaded successfully ({blogPostId})";
+                }
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = ex.Message;
             }
 
             return View();
